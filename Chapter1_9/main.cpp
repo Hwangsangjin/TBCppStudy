ab6b2d76e1fe36a0d17fbca4760dd80aaa392cdd
상진 <ghkdtkdwls88@gmail.com>
2022-12-03 07:27:28 +0900

    기본적인 서식 맞추기

---
+++ Chapter1_9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int Add(int x, int y)
+{
+	return x + y;	// 여백
+}
+
+int main()
+{
+	int my_v		= 1;
+	int x			= 4;
+	int num_apples	= 123;
+
+	// 주석
+	int x = 1 + 2;
+
+	// 주석
+	int y = 3 + 4;
+
+	return 0;
+}
