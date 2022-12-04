0696bf9605538c08993b32b09f1c540947da3b5d
hwangsangjin <ghkdtkdwls88@gmail.com>
2022-12-04 18:54:09 +0900

    무치형 (보이드, Void)

---
+++ Chapter02_04/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void MyFunction()
+{
+
+}
+
+int main()
+{
+	int i = 123;
+	float f = 123.456f;
+
+	//void my_void;
+
+	void* my_void;
+
+	my_void = (void*)&i;
+	my_void = (void*)&f;
+
+	return 0;
+}
