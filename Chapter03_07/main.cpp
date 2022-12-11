6a14771f16e26294e02162543f8a30e39df5fc6a
hwangsangjin <ghkdtkdwls88@gmail.com>
2022-12-11 22:54:28 +0900

    이진수 Binary Numbers

---
+++ Chapter03_07/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	0
+	1
+	10 = 2^1*1 + 2^0*0 = 2
+	11 = 2^1*1 + 2^0*1 = 3
+
+	  1
+	  11
+	+  1
+	----
+	 100
+	*/
+
+	return 0;
+}
