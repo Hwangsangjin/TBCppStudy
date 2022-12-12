5050f2939565d5416f53492cb1e3b9ef819d4522
hwangsangjin <ghkdtkdwls88@gmail.com>
2022-12-13 00:09:44 +0900

    비트단위 연산자 Bitwise Operators

--- Chapter03_08/main.cpp
+++ Chapter03_08/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
@@ -6,6 +7,22 @@ int main()
 {
 	// <<, >>
 	// ~, &, |, ^
+
+	unsigned int a = 3;
+	cout << std::bitset<4>(a) << endl;
+
+	unsigned int b = a << 1;
+	cout << std::bitset<4>(b) << endl;
+
+	unsigned int c = ~b;
+	cout << std::bitset<4>(c) << endl;
+
+	unsigned int x = 0b1100;
+	unsigned int y = 0b0110;
+
+	cout << std::bitset<4>(x & y) << endl;
+	cout << std::bitset<4>(x | y) << endl;
+	cout << std::bitset<4>(x ^ y) << endl;
 
 	return 0;
 }
