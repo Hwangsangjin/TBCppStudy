d5a12a392694099680b25bf5e3a35c764c89427c
hwangsangjin <ghkdtkdwls88@gmail.com>
2023-01-07 09:24:20 +0900

    자료형에게 가명 붙여주기

---
+++ Chapter04_09/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	typedef float distance_t;
+
+	float myDistance;
+	distance_t homeToWork;
+	distance_t homeToSchool;
+
+	//typedef vector<pair<string, int>> pairList_t;
+	using pairList_t = vector<pair<string, int>>;
+
+	pairList_t pairList1;
+	pairList_t pairList2;
+	
+	return 0;
+}
