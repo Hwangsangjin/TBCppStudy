6a8a1ad4ada4b80cbd71ec8197ec7204ccd94c90
hwangsangjin <ghkdtkdwls88@gmail.com>
2023-01-07 10:59:32 +0900

    제어 흐름 개요 Control flow

---
+++ Chapter05_01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	cout << "I love you" << endl;
+
+	exit(0);
+
+	// ...
+
+	/*
+	cout << "I love you, too" << endl;
+
+	return 0;
+	*/
+}
