10f03c18106f8a6ce7e86d69d082d0f3c3aa4c0e
hwangsangjin <ghkdtkdwls88@gmail.com>
2023-01-14 18:07:13 +0900

    메모리 동적 할당 new와 delete

---
+++ Chapter06_11/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int var;
+	var = 7;
+
+	int* ptr = new int;
+	*ptr = 7;
+
+	cout << ptr << endl;
+	cout << *ptr << endl;
+
+	delete ptr;
+	ptr = nullptr;
+
+	cout << "After delete" << endl;
+	if (ptr != nullptr)
+	{
+		cout << ptr << endl;
+		cout << *ptr << endl;
+	}
+
+	return 0;
+}
