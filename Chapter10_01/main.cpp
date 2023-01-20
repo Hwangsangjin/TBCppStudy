bbef3058a2b1081f555c0b5380c5d2bd90f70838
hwangsangjin <ghkdtkdwls88@gmail.com>
2023-01-21 01:51:59 +0900

    객체들의 관계

---
+++ Chapter10_01/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*				관계를 표현하는 동사				예시
+	구성 (요소)			Part-of				두뇌는 육체의 일부이다.
+	Composition
+
+	집합					Has-a				어떤 사람이 자동차를 가지고 있다.
+	Aggregation
+	
+	연계, 제휴			Uses-a				환자는 의사의 치료를 받는다.
+	Association								의사는 환자들로부터 치료비를 받는다.
+
+	의존
+	Dependency			Depends-on			나는 (다리가 부러져서 한 달 동안) 목발을 짚었다.
+	*/
+
+	return 0;
+}
